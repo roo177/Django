97230a35c0f00b0e77fc8949ae4a0065c5202525
roo177 <mahirer@gmail.com>
2023-05-05 19:24:43 +0300

    update

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,23 +1,16 @@
-# This file contains the WSGI configuration required to serve up your
-# web application at http://roo17.pythonanywhere.com/
-# It works by setting the variable 'application' to a WSGI handler of some
-# description.
-#
-# The below has been auto-generated for your Django project
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
 
 import os
-import sys
 
-# add your project directory to the sys.path
-project_home = '/home/roo17/mysite'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
-
-# set environment variable to tell django where your settings.py is
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
-
-# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
